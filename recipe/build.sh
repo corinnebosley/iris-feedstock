8fe3489e7a80242b5a3340c792163351d4eda997
Filipe Fernandes <ocefpaf@gmail.com>
2018-02-15 10:34:38 -0800

    build wheels instead of egg

--- recipe/build.sh
+++ recipe/build.sh
@@ -10,8 +10,7 @@ echo "udunits2_path = $PREFIX/lib/libudunits2${SHLIB_EXT}" >> $SITECFG
 
 rm -rf lib/iris/tests/results lib/iris/tests/*.npz
 
-python setup.py install --single-version-externally-managed --record record.txt
-
+python -m pip install --no-deps --ignore-installed .
 
 # Without this line, the OS X build fails reproducibly with
 # https://github.com/conda-forge/iris-feedstock/pull/2#issuecomment-217468453.
